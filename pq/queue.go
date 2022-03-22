55ccca7932e924c08c2d91d576dc3ed11ed1dff2
enhanced233 <amir233@gmail.com>
2022-03-22 18:24:18 +0200

    made Queue hold a list instead and added binary search when pushing new data

--- pq/queue.go
+++ pq/queue.go
@@ -1,27 +1,56 @@
 package pq
 
+type node struct {
+	data     interface{}
+	priority int
+}
+
 type Queue struct {
-	head *node
+	NodeList []*node
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.NodeList) == 0
+}
+
+func searchPosition(list []*node, priority int) int {
+	n := len(list)
+	max := n
+	min := 0
+	current := n / 2
+	for (max - min) > 0 {
+		if list[current].priority <= priority {
+			min = current
+			current += (max - min) / 2
+		} else {
+			max = current
+			current -= (max - min) / 2
+		}
+		if (max - min) == 1 {
+			if list[min].priority <= priority {
+				return max
+			}
+			return min
+		}
+	}
+	return max
 }
 
 func (q *Queue) Insert(data interface{}, priority int) {
 	newNode := &node{data: data, priority: priority}
 	if q.IsEmpty() {
-		q.head = newNode
+		q.NodeList = append(q.NodeList, newNode)
 	} else {
-		q.head = q.head.push(newNode)
+		pos := searchPosition(q.NodeList, priority)
+		q.NodeList = append(q.NodeList[:pos], append([]*node{newNode}, q.NodeList[pos:]...)...)
 	}
 }
 
 func (q *Queue) Pull() interface{} {
 	if q.IsEmpty() {
-		panic("Error: the queue is empty, cannot pull data!!")
+		return nil
 	}
-	data := q.head.getData()
-	q.head = q.head.next
+	data := q.NodeList[0].data
+	q.NodeList = q.NodeList[1:]
 	return data
-}
-
-func (q *Queue) IsEmpty() bool {
-	return q.head == nil
 }
